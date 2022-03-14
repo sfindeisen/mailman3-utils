a76be605f3cd782634d9e9ddd72f5c3915860d87
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-03-14 17:41:05 +0100

    hello world

---
+++ common.py
@@ -0,0 +1,37 @@
+import argparse
+import logging
+import os
+import sys
+
+import mailmanclient
+
+def new_client():
+    client_pass = os.getenv('MAILMAN_REST_API_PASS')
+    if client_pass:
+        client = mailmanclient.Client('http://localhost:8001/3.1', 'restadmin', client_pass)
+        logging.debug('Mailman client: {}'.format(client))
+        return client
+    else:
+        raise Exception("Environment variable MAILMAN_REST_API_PASS not set!")
+
+def setup_logging(verbose=False):
+    log_format = '[{asctime}] {levelname:8} {threadName:<14} {message}'
+    logging.basicConfig(stream=sys.stderr, level=(logging.DEBUG if verbose else logging.INFO), format=log_format, style='{')
+
+def setup_args():
+    parser = argparse.ArgumentParser(
+        add_help=True, allow_abbrev=False, epilog="""This program comes with ABSOLUTELY NO WARRANTY.""")
+
+    parser.add_argument("--verbose",
+                        required=False,
+                        action="store_true",
+                        default=False,
+                        help="verbose processing")
+
+    # Parse command line arguments
+    args = parser.parse_args()
+    return args
+
+def program_start():
+    args = setup_args()
+    setup_logging(args.verbose)
---
+++ list_domains.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import logging
+
+import common
+
+if __name__ == "__main__":
+
+    common.program_start()
+    logging.info("hello world!")
