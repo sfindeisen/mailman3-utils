8be42e811fdf8e257c3e650307867493b9b16cc4
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-03-18 16:44:08 +0100

    add 2 comments

--- show_list.py
+++ show_list.py
@@ -6,12 +6,14 @@ import common
 
 if __name__ == "__main__":
 
+    # setup args and logging
     args = common.setup_args(domain=common.ArgMod.REQUIRED, llist=common.ArgMod.REQUIRED)
     common.setup_logging(args.verbose)
 
     fqdn_listname = "{}@{}".format(args.llist, args.domain)
     logging.info("searching for list: {} ...".format(fqdn_listname))
 
+    # setup client
     client = common.new_client()
     lists  = client.lists
     lists  = list(filter(lambda x : (fqdn_listname == x.fqdn_listname), lists))
