0f38bd0358b3eba02ce4bcaf6d6ea383b0fdd843
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-03-18 19:13:40 +0100

    implement update_list

---
+++ update_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import logging
+
+import common
+
+if __name__ == "__main__":
+
+    # setup args and logging
+    args = common.setup_args(domain=common.ArgMod.REQUIRED, llist=common.ArgMod.REQUIRED)
+    common.setup_logging(args.verbose)
+
+    fqdn_listname = "{}@{}".format(args.llist, args.domain)
+    logging.info("searching for list: {} ...".format(fqdn_listname))
+
+    # setup client
+    client = common.new_client()
+
+    # fetch list
+    llist = common.fetch_list(client, fqdn_listname)
+    common.apply_list_settings(llist)
