d4a5c599593c1a149719c4731e87c097d80837ab
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-03-18 17:01:27 +0100

    implement filtering by domain (list_lists)

--- list_lists.py
+++ list_lists.py
@@ -2,16 +2,32 @@
 
 import common
 
+def fetch_lists(client, domain):
+    if domain:
+        doms = client.domains
+        doms = list(filter(lambda x: (domain == x.mail_host), doms))
+
+        if doms:
+            domc = len(doms)
+            if (2 <= domc):
+                raise Exception("Multiple matches for domain: {}, bailing out".format(domain))
+            else:
+                return doms[0].lists
+        else:
+            raise Exception("Domain not found: {}".format(domain))
+    else:
+        return client.lists
+
 if __name__ == "__main__":
 
-
     # setup args and logging
-    args = common.setup_args()
+    args = common.setup_args(domain=common.ArgMod.OPTIONAL)
     common.setup_logging(args.verbose)
 
     # setup client
     client = common.new_client()
 
-    lists = client.lists
+    # print lists
+    lists = fetch_lists(client, args.domain)
     for lx in lists:
         print(lx.fqdn_listname)
