6f4ddaa892764e439317ecd09d4842ee258b49e5
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-03-22 14:13:13 +0100

    implement create_domain

---
+++ create_domain.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import common
+import logging
+
+if __name__ == "__main__":
+
+    # setup args and logging
+    args = common.setup_args(domain=common.ArgMod.REQUIRED)
+    common.setup_logging(args.verbose)
+
+    logging.info("create domain: {} ...".format(args.domain))
+
+    # setup client
+    client = common.new_client()
+
+    # create domain
+    client.create_domain(args.domain)
